e0e00767fd5c7bded71696756b2c9a4296a106d8
Ivan5646 <Mike000027@yandex.ru>
2017-03-16 00:38:15 +0700

    active menu item2

--- wp-content/themes/wpplay/js/common.js
+++ wp-content/themes/wpplay/js/common.js
@@ -4,12 +4,14 @@
 // }
 
 function myFunction(item) {
-  $(item).addClass("active");
-  console.log($(item));
+  $(item).find('a').addClass("active");
+  var myVar = $(item).find('a');
+  console.log(myVar);
 }
 
 var menu = document.getElementsByClassName("sf-menu");
 
 for (var i = 0; i < menu.length; i++) {
-  menu[i].addEventListener("click", myFunction(menu[i]));
+  menu[i].addEventListener("click", myFunction);
+
 }
